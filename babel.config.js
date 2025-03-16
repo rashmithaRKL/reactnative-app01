7901a77bb58b0189a66c37aeaea555153fa23c05
CodeSandbox <support@codesandbox.io>
2025-03-16 08:57:01 +0000

    fix: Update babel config and dependencies for proper build

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'nativewind/babel',
-    'react-native-reanimated/plugin',
+    ['nativewind/babel'],
+    ['react-native-reanimated/plugin'],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
   ],
 };
